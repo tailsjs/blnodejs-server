d639daae104143ed005225e15da125dc9b2a423d
тейлс <49571184+tailsjs@users.noreply.github.com>
2022-06-04 02:26:47 +0400

    scores fix

--- route/scores/get_scores.js
+++ route/scores/get_scores.js
@@ -3,8 +3,14 @@ module.exports = {
     func: async (q, s, db) => {
         let users = db.get("users").find(e => e.name != "" && e.banned == false).value()
         let userString = []
-        if(!users || !Array.isArray(users))
+
+        if(!users)return s.send("-1")
+
+        if(!Array.isArray(users))
             users = [ users ]
+
+        users.sort((a, b) => Number(a.score) - Number(b.score))
+
         for(let i of users)
             userString.push(`${i.name};${i.udid};${i.score};${i.context}`) // it's very safe put udid to scores.......
         
